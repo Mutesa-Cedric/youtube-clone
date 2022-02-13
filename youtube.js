b1bb4ed5f3090347e8320a31ad54c9d82d4c1ff0
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-02-13 08:17:33 +0200

    full version

---
+++ youtube.js
@@ -0,0 +1,11 @@
+// const header = document.querySelector(".headercontainer")
+
+// window.addEventListener("scroll", fixheader);
+
+// function fixheader() {
+
+//     if (window.scrollY > 0) {
+
+//         header.classlist.add("onscroll");
+//     }
+// }
